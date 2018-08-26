feb9f3ff46b6e0b3c3b91568d0e522c0de4384ce
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-08-26 12:22:49 -0400

    Volume Chart

--- src/charts/charts.js
+++ src/charts/charts.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 
 export function lineChart(element, data) {
-    const days = data.data;
+    const days = (data.range) ? data.data : data;
 
     if (element.innerHTML) {
         element.innerHTML = '';
@@ -14,32 +14,46 @@ export function lineChart(element, data) {
     const svgWidth = width - margin.left - margin.right;
     const svgHeight = height - margin.top - margin.bottom;
 
-    const firstDay = days[1];
+    const firstDay = days[0];
     const lastDay = days[days.length - 1];
 
-    const maxClose = d3.max(days, (d) => d.close);
+    const [minClose, maxClose] = d3.extent(days, (d) => d.close);
+    const closeDelta = (maxClose - minClose);
+
+    const [minVolume, maxVolume] = d3.extent(days, (d) => d.volume);
 
     const x = d3.scaleTime()
         .domain([new Date(firstDay.date), new Date(lastDay.date)])
         .range([0, svgWidth]);
 
     const y = d3.scaleLinear()
-        .domain([0, maxClose])
-        .range([svgHeight, 0]);
+        .domain([(minClose - Math.ceil((closeDelta * .1))), maxClose + Math.ceil((closeDelta * .1))])
+        .range([svgHeight * .65, 0]);
+
+    const xVolumeScale = d3.scaleBand()
+        .rangeRound([0, svgWidth])
+        .domain(days.map(d => d.date))
+        .padding(.3);
+
+    const yVolumeScale = d3.scaleLinear()
+        .domain([minVolume, maxVolume])
+        .range([svgHeight, svgHeight * .70]);
 
     const line = d3.line()
-        .x(function (d) {
+        .x((d) => {
             const date = new Date(d.date);
             return x(date);
         })
-        .y(function (d) {
+        .y((d) => {
             return y(d.close);
         });
 
     const xAxis = d3.axisBottom(x)
         .ticks(4);
 
-    const yAxis = d3.axisLeft(y);
+    const yAxis = d3.axisLeft(y).tickSizeOuter(0);
+
+    const yVolume = d3.axisLeft(yVolumeScale).ticks(3).tickSizeOuter(0).tickFormat(d3.format('~s'));
 
     const svg = d3.select(element)
         .append('svg')
@@ -58,15 +72,34 @@ export function lineChart(element, data) {
         .attr('class', 'y axis')
         .call(yAxis);
 
+    svg.append('g')
+        .attr('class', 'volume axis')
+        .call(yVolume);
+
     svg.append("path")
         .datum(days)
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
+        .attr("stroke", '#0d9da8')
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
         .attr("stroke-width", 1.5)
         .attr("d", line);
 
-    console.log(data);
-
+    svg.selectAll(".bar")
+        .data(days)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .style('fill', '#0d9da8')
+        .style('opacity', .5)
+        .attr("x", function (d, i) {
+            const date = new Date(d.date);
+            return x(date);
+        })
+        .attr("y", function (d) {
+            return yVolumeScale(d.volume);
+        })
+        .attr("width", xVolumeScale.bandwidth())
+        .attr("height", function (d) {
+            return svgHeight - yVolumeScale(d.volume);
+        });
 }
--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -2,60 +2,73 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 import {withStyles} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+
 import AppBar from '@material-ui/core/AppBar';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 import grey from '@material-ui/core/colors/grey';
 
 import {lineChart} from '../charts/charts';
 
 const styles = theme => ({
-    grow: {
-        flexGrow: 1
-    },
-    lineChart: {
-        '& svg.line-chart': {
-            '& .axis': {
-                '& path, & line': {
-                    stroke: grey[400]
-                },
-                ' & text': {
-                    fill: grey[600]
+        grow: {
+            flexGrow: 1
+        },
+        lineChart: {
+            '& svg.line-chart': {
+                '& .axis': {
+                    '& path, & line': {
+                        stroke: grey[400]
+                    },
+                    ' & text': {
+                        fill: grey[600]
+                    }
                 }
             }
+        },
+        formControl: {
+            margin: theme.spacing.unit
+        },
+        group: {
+            flexDirection: 'row',
+            justifyContent: 'flex-end'
+        },
+        legendLabel: {
+            margin: theme.spacing.unit
         }
     }
-});
+);
 
 class ChartContainer extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.handleDateRangeChange = this.handleDateRangeChange.bind(this);
+
+        this.state = {
+            range: 'ytd'
+        }
+    }
+
     componentDidMount() {
         const {symbol} = this.props;
-        const {element} = this;
-
         if (symbol) {
-            axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/chart/dynamic`)
-                .then(function (stocks) {
-                    lineChart(element, stocks.data);
-                });
+            this.fetchData();
         }
     }
 
     componentDidUpdate(prevProps, prevState, prevContext) {
         const {symbol} = this.props;
-        const {element} = this;
 
         if (prevProps.symbol !== symbol) {
-            axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/chart/dynamic`)
-                .then(function (stocks) {
-
-                    window.addEventListener('resize', () => {
-                        lineChart(element, stocks.data);
-                    }, false);
-
-                    lineChart(element, stocks.data);
-                });
+            this.fetchData()
         }
     }
 
@@ -66,6 +79,55 @@ class ChartContainer extends Component {
         window.removeEventListener('resize', () => {
             lineChart(element, stocks.data);
         });
+    }
+
+    fetchData() {
+        const {symbol} = this.props;
+        const {range} = this.state;
+
+        const {element} = this;
+
+        return axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/chart/${range}`)
+            .then(function (stocks) {
+
+                window.addEventListener('resize', () => {
+                    lineChart(element, stocks.data);
+                }, false);
+
+                lineChart(element, stocks.data);
+            });
+    }
+
+    handleDateRangeChange(event) {
+        this.setState({range: event.target.value}, () => {
+            this.fetchData();
+        });
+    }
+
+    renderDateRangeControl() {
+        const dateRange = ['1m', '3m', '6m', 'ytd', '1y', '2y'];
+        const {classes} = this.props;
+        const {range} = this.state;
+
+        return <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend">Date Range</FormLabel>
+            <RadioGroup
+                aria-label="Gender"
+                name="gender1"
+                className={classes.group}
+                value={range}
+                onChange={this.handleDateRangeChange}>
+
+                {dateRange.map((range) => {
+                    return <FormControlLabel value={range}
+                                             key={range}
+                                             label={range}
+                                             control={<Radio/>}/>
+
+                })}
+
+            </RadioGroup>
+        </FormControl>
     }
 
     render() {
@@ -79,7 +141,17 @@ class ChartContainer extends Component {
                         <Typography variant='subheading'>Chart - {symbol}</Typography>
                     </Toolbar>
                 </AppBar>
+
+                {this.renderDateRangeControl()}
+
+                <div className={classes.legendLabel} style={{marginBottom: '-8px'}}>
+                    <Typography variant='caption'>Close Price ($)</Typography>
+                </div>
+
                 <div className={[classes.grow, classes.lineChart].join(' ')} ref={element => this.element = element}/>
+                <div className={classes.legendLabel} style={{textAlign: 'right', marginTop: '-16px'}}>
+                    <Typography variant='caption'>Time</Typography>
+                </div>
             </React.Fragment>
 
         );
